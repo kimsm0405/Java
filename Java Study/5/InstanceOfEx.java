3b1a515e663d3f810603036799299f7c0a81c3f7
Kim Seong Min <ksm990405@gmail.com>
2025-06-01 15:58:34 +0900

    Add files via upload

---
+++ Java Study/5/InstanceOfEx.java
@@ -0,0 +1,24 @@
+
+//class Person1 {}
+//class Student1 extends Person1 {}
+//class REsearcher extends Person1 {}
+//class Professor extends Researcher {}
+//
+//public class InstanceOfEx {
+//	static void print(Person1 P) {
+//		if(p instanceof Person1)
+//			System.out.println("Person ");
+//		if(p instanceof Student1)
+//			System.out.println("Student ");
+//		if(p instanceof Researcher)
+//			System.out.println("Researcher ");
+//		if(p instanceof Professor)
+//			System.out.println("Professor ");
+//		System.out.println();
+//	}
+//	public static void main(String[] args) {
+//		System.out.println("new Student() -> \t"); print(new Student());
+//		System.out.println("new Researcher() -> \t"); print(new Researcher());
+//		System.out.println("new Professor() -> \t"); print(new Professor());
+//	}
+//}
